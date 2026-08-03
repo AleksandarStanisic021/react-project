9aa6ee5cb05f7f6ffaaa380a98ed2c22046ff92c
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-03 16:02:20 +0200

    fix: standardize Greet component prop syntax

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   const age = 100;
   return (
     <>
-      <Greet name="Sasa" age={100} />
-      <Greet name="John" age={25} />
-      <Greet name="Jane" age={30} />
+      <Greet name={"Sasa"} age={100} />
+      <Greet name={"John"} age={25} />
+      <Greet name={"Jane"} age={30} />
       <p>
         {name} is {age} years old.
       </p>
