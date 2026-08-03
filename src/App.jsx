b2d08bceaf8fa288b769ee76a2db83967e334ba1
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-03 16:39:20 +0200

    fixing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import Greet from "./Greet";
+import { useState } from "react";
 
 function App() {
-  const showInfo = false;
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleClick = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
-    <>
+    <div>
+      <button onClick={handleClick}>Click Me</button>
       {showInfo && <Greet name={"Sasa"} age={100} />}
-      {showInfo ? <Greet name={"John"} age={25} /> : "Nothing to show"}
-    </>
+    </div>
   );
 }
 
