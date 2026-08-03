6afb30bab46fb3fc90f0c62b52e1a278d30dc4d0
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-03 15:45:16 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 
+function Greet() {
+  return <h1>Greet !</h1>;
+}
+
 function App() {
   const name = "Sasa";
   const age = 100;
   return (
     <>
+      <Greet />
+      <Greet />
+      <Greet />
       <p>
         {name} is {age} years old.
       </p>
