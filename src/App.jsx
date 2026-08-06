9b8f6ed9fc76ec83a40ed1139ddf2fc602becd1f
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-06 14:29:28 +0200

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import "./App.css";
 import Greet from "./Greet";
 import Todo from "./Todo";
 import SignForm from "./SignForm";
+import { Routes, Route } from "react-router-dom";
 
 import { useState } from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
 
 function App() {
   const [showInfo, setShowInfo] = useState(false);
@@ -15,6 +31,11 @@ function App() {
 
   return (
     <div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
       <button onClick={handleClick}>Click Me</button>
       {showInfo && <Greet name={"Sasa"} age={100} />}
       <div>
