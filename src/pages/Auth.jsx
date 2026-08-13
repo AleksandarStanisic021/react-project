96a94f96b6c0d89807773f5b2f93b8d5d5ecc964
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-13 16:45:42 +0200

    styling form

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,38 @@
 export default function Auth() {
   return (
-    <>
-      <h1>Auth Page</h1>
-      <p>This is the authentication page.</p>
-    </>
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          <h1 className="page-title">Sign Up</h1>
+          <form action="" className="auth-form">
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-input"
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="form-input"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
